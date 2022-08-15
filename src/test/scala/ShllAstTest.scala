f1eae95512f2e127c9d72c861833d452ba129ac5
JakkuSakura <qjk2001@gmail.com>
2022-08-15 11:37:18 +0800

    chore: PrettyPrinter

--- src/test/scala/ShllAstTest.scala
+++ src/test/scala/ShllAstTest.scala
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import shll.ShllParser
+import shll.{PrettyPrinter, ShllParser}
 
 class ShllAstTest {
   @Test def test_parser(): Unit = {
     val t = ShllParser().parse("(block (A) (B))")
-    println(t)
+    println(PrettyPrinter().print(t))
   }
 
 }
