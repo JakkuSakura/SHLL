6d84bd2723b2eed37e511b0ce796551492c0ebe2
qiujiangkun <qjk2001@gmail.com>
2022-09-23 00:47:58 +0800

    cleanup codebase

--- tests/test_jsonify.py
+++
@@ -1,9 +0,0 @@
-from unidef.languages.javascript.jsonify import Jsonify, JsonObject, JsonProperty, JsonRawValue
-
-
-def test_jsonify():
-    jsonify = Jsonify()
-    assert jsonify({}) == JsonObject([])
-    assert jsonify({
-        'test': 1
-    }) == JsonObject([JsonProperty(JsonRawValue('test'), JsonRawValue(1))])
