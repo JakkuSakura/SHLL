a8e5df2b79cdd41daa39f8f2efa42f79b589fe9d
JakkuSakura <qjk2001@gmail.com>
2022-09-23 15:17:19 +0800

    refactor: refactor tests

--- src/test/scala/shll/ShllAstTest.scala
+++ src/test/scala/shll/ShllAstTest.scala
@@ -2,18 +2,40 @@ package shll
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import shll.ast.AST
 import shll.backends.PrettyPrinter
 import shll.frontends.ShllLexerAndParser
 import shll.optimizers.Specializer
 
 class ShllAstTest {
+  val pp: PrettyPrinter = PrettyPrinter()
+  val showProgress = true
+  def printAst(input: AST): Unit = {
+    println(pp.print(input))
+  }
+
+  def specializedEquals(input: String, expected: String): Unit = {
+    if (showProgress)
+        println(s"Parsing $input")
+    val ast = ShllLexerAndParser().parse(input)
+    if (showProgress)
+        println(s"Specializing " + pp.print(ast))
+    val optimized = Specializer().specialize(ast)
+    if (showProgress)
+        println(s"Optimized " + pp.print(optimized))
+    val exp = ShllLexerAndParser().parse(expected)
+    if (exp != optimized)
+      if (showProgress)
+          println(s"Expected " + pp.print(exp))
+      assertEquals(exp, optimized)
+  }
   @Test def testParser(): Unit = {
     val t = ShllLexerAndParser().parse("(block (A) (B))")
-    println(PrettyPrinter().print(t))
+    printAst(t)
   }
 
   @Test def testSpecializer(): Unit = {
-    val src = ShllLexerAndParser().parse(
+    specializedEquals(
       """
         |(block
         |   (def-fun foo (list (field a [int])) [int]
@@ -21,40 +43,32 @@ class ShllAstTest {
         |   )
         |   (foo 1)
         |)
-        |""".stripMargin)
-    println(PrettyPrinter().print(src))
-    val specialized = Specializer().specialize(src)
-    println(PrettyPrinter().print(specialized))
-    val expected = ShllLexerAndParser().parse(
+        |""".stripMargin,
       """
         |(block
         |  (def-fun foo_0 (list) [int] a)
         |  (def-fun foo (list (field a [int])) [int] a)
         |  (foo_0)
         |)
-        |""".stripMargin)
-    assertEquals(expected, specialized)
+        |""".stripMargin
+    )
   }
 
   @Test def testStruct(): Unit = {
-    val src = ShllLexerAndParser().parse(
+    specializedEquals(
       """
         |(block
         |   (def-struct Foo (list (field a [int])))
         |   (select (Foo a=1) a)
         |)
-        |""".stripMargin)
-    println(PrettyPrinter().print(src))
-    val specialized = Specializer().specialize(src)
-    println(PrettyPrinter().print(specialized))
-    val expected = ShllLexerAndParser().parse(
+        |""".stripMargin,
       """
         |(block
         |  (def-struct Foo (list (field a [int])))
         |  1
         |)
-        |""".stripMargin)
-    assertEquals(expected, specialized)
+        |""".stripMargin
+    )
   }
 
 }
