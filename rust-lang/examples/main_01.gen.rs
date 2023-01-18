6f52736980a99a7ad84daa7fc396f33d90359cf9
JakkuSakura <qjk2001@gmail.com>
2023-01-19 07:08:53 +0800

    fix: parse rust program & bugfixes

---
+++ rust-lang/examples/main_01.gen.rs
@@ -0,0 +1,12 @@
+fn main() -> () {
+    print(inc(1i64));
+}
+fn inc(i: i64) -> i64 {
+    i + 1i64
+}
+fn double(i: i64) -> i64 {
+    i * 2i64
+}
+fn print(i: i64) -> () {
+    println!("{}", i)
+}
--- rust-lang/src/main.rs
+++ rust-lang/src/main.rs
@@ -5,17 +5,23 @@ use rust_lang::RustSerde;
 
 fn main() -> Result<()> {
     setup_logs(LogLevel::Trace)?;
-    let code = RustSerde.deserialize(
-        r#"
-    fn main() {
-        print(1);
-    } 
-    "#,
-    )?;
+    let mut base = std::path::Path::new(file!()).to_path_buf();
+    base.pop();
+    base.push("../examples");
+    let file = "main_01.rs";
+    let mut file_in = base.clone();
+    file_in.push(file);
+
+    let mut file_out = base.clone();
+    file_out.push(file.replace(".rs", ".gen.rs"));
+
+    let file_content = std::fs::read_to_string(file_in)?;
+    let code = RustSerde.deserialize(&file_content)?;
     info!("Code: {:?}", code);
     let code = RustSerde.serialize(&code)?;
     info!("Code: {}", code);
     let code = format_code(&code)?;
     info!("Code: {}", code);
+    std::fs::write(file_out, &code)?;
     Ok(())
 }
