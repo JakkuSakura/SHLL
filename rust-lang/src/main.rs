a8d4f9ec37a471fc83da3604bdf4a554521f65cb
JakkuSakura <qjk2001@gmail.com>
2023-01-19 12:08:12 +0800

    chore: batch process examples

--- rust-lang/src/main.rs
+++ rust-lang/src/main.rs
@@ -8,20 +8,26 @@ fn main() -> Result<()> {
     let mut base = std::path::Path::new(file!()).to_path_buf();
     base.pop();
     base.push("../examples");
-    let file = "main_01.rs";
-    let mut file_in = base.clone();
-    file_in.push(file);
+    let dir = std::fs::read_dir(&base)?;
+    for file in dir {
+        let file = file?;
+        let file = file.file_name().as_os_str().to_string_lossy().to_string();
+        if file.ends_with(".rs") && !file.contains(".gen") {
+            let mut file_in = base.clone();
+            file_in.push(&file);
 
-    let mut file_out = base.clone();
-    file_out.push(file.replace(".rs", ".gen.rs"));
-
-    let file_content = std::fs::read_to_string(file_in)?;
-    let code = RustSerde.deserialize(&file_content)?;
-    info!("Code: {:?}", code);
-    let code = RustSerde.serialize(&code)?;
-    info!("Code: {}", code);
-    let code = format_code(&code)?;
-    info!("Code: {}", code);
-    std::fs::write(file_out, &code)?;
+            let mut file_out = base.clone();
+            file_out.push(file.replace(".rs", ".gen.rs"));
+            info!("{:?} => {:?}", file_in, file_out);
+            let file_content = std::fs::read_to_string(file_in)?;
+            let code = RustSerde.deserialize(&file_content)?;
+            // info!("Code: {:?}", code);
+            let code = RustSerde.serialize(&code)?;
+            // info!("Code: {}", code);
+            let code = format_code(&code)?;
+            // info!("Code: {}", code);
+            std::fs::write(file_out, &code)?;
+        }
+    }
     Ok(())
 }
