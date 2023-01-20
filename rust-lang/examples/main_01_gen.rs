d71953a999978ee022dae1d1cf53789d96eabf61
qiujiangkun <qjk2001@gmail.com>
2023-01-20 12:05:43 +0800

    high level generics WIP

--- rust-lang/examples/main_01_gen.rs
+++ rust-lang/examples/main_01_gen.rs
@@ -1,9 +1,3 @@
-fn inc(i: i64) -> i64 {
-    i + 1i64
-}
-fn double(i: i64) -> i64 {
-    i * 2i64
-}
 fn print(i: i64) -> () {
     println!("{}", i)
 }
--- rust-lang/examples/main_02_gen.rs
+++ rust-lang/examples/main_02_gen.rs
@@ -1,23 +1,16 @@
 use std::ops::Add;
-fn do_op(a: i64, b: i64, c: f64, d: f64, op: fn(i64, i64) -> i64) -> () {
-    print(op(a, b));
-    print(op(c, d));
-}
-fn add<T: Add>(a: T, b: T) -> T {
-    a + b
-}
 fn print(i: i64) -> () {
     println!("{}", i)
 }
 fn main() -> () {
     do_op_2();
 }
+fn add_0() -> T {
+    1i64 + 2i64
+}
 fn do_op_2() -> () {
     print(add_0());
     print(add_1());
-}
-fn add_0() -> T {
-    1i64 + 2i64
 }
 fn add_1() -> T {
     3f64 + 4f64
