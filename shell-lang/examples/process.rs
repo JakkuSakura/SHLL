9f1720c596b84b0274adcb463ee3fd12d7fd847c
JakkuSakura <qjk2001@gmail.com>
2023-02-21 23:31:59 +0800

    chore: shell lang

---
+++ shell-lang/examples/process.rs
@@ -0,0 +1,9 @@
+include!("defs/mod.rs");
+use shell_lang::*;
+
+fn main() {
+    let src = SourceProcess::spawn("src", 1);
+    let add = AddProcess::spawn("add");
+    let dest = SinkProcess::spawn("dest");
+    let _ = pipe!(src | add | dest).start();
+}
