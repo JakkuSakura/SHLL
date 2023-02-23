14660b1eead8a2ee3fffbc7da18dfc9476bf6765
JakkuSakura <qjk2001@gmail.com>
2023-02-23 22:38:21 +0800

    chore: use move semantics in shell

--- shell-lang/examples/current_grammar.rs
+++ shell-lang/examples/current_grammar.rs
@@ -3,9 +3,9 @@ use shell_lang::starter::TryStarter;
 use shell_lang::*;
 
 fn main() {
-    let src = SourceProcess::spawn("src", 1);
-    let adder = AddProcess::spawn("adder");
-    let dest = SinkProcess::spawn("dest");
+    let src = &SourceProcess::spawn("src", 1);
+    let adder = &AddProcess::spawn("adder");
+    let dest = &SinkProcess::spawn("dest");
     let add_five = adder.add(5);
     pipe!(src | add_five | dest).start();
     pipe!(src | adder | dest).start();
--- shell-lang/examples/ideal_grammar.rs
+++ shell-lang/examples/ideal_grammar.rs
@@ -3,13 +3,11 @@ use shell_lang::starter::TryStarter;
 use shell_lang::*;
 
 fn main() {
-    let src = SourceProcess::spawn("src", 1);
+    let src = &SourceProcess::spawn("src", 1);
     let adder = AddProcess::spawn("adder");
-    let dest = SinkProcess::spawn("dest");
+    let dest = &SinkProcess::spawn("dest");
 
     // declarative macro can't create new idents
     shell!(src | adder.add(5) | dest);
     shell!(src | adder.add(6) | dest);
 }
-// TODO: use move semantics in shell
-// to share use &, to cross thread use &Arc
