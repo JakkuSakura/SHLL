4c60e1b5a79a23f5f706f4d4b76dc3b2ae290a97
qiujiangkun <qjk2001@gmail.com>
2023-08-17 15:28:10 +0800

    [dev] fix tests and CI

--- rust-lang/examples/main_02_higher_kinded_function_gen.rs
+++ rust-lang/examples/main_02_higher_kinded_function_gen.rs
@@ -7,69 +7,69 @@ fn print(i: impl Display) {
     println!("{}", i)
 }
 fn main() {
-    let c = 3f64;
-    let a = 1i64;
     let d = 4f64;
+    let a = 1i64;
     let b = 2i64;
+    let c = 3f64;
     let op = add;
     {
-        let c = 3f64;
-        let a = 1i64;
         let d = 4f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let op = add;
         let i = op(a, b);
         print(i)
     };
     {
-        let c = 3f64;
-        let a = 1i64;
         let d = 4f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let op = add;
         let i = op(c, d);
         print(i)
     };
 }
 pub fn do_op_2() {
-    let c = 3f64;
-    let a = 1i64;
     let d = 4f64;
+    let a = 1i64;
     let b = 2i64;
+    let c = 3f64;
     let op = add;
     {
-        let c = 3f64;
-        let a = 1i64;
         let d = 4f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let op = add;
         let i = op(a, b);
         print(i)
     };
     {
-        let c = 3f64;
-        let a = 1i64;
         let d = 4f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let op = add;
         let i = op(c, d);
         print(i)
     };
 }
 pub fn print2_0() {
-    let c = 3f64;
-    let a = 1i64;
     let d = 4f64;
+    let a = 1i64;
     let b = 2i64;
+    let c = 3f64;
     let op = add;
     let i = op(a, b);
     print(i)
 }
 pub fn print2_1() {
-    let c = 3f64;
-    let a = 1i64;
     let d = 4f64;
+    let a = 1i64;
     let b = 2i64;
+    let c = 3f64;
     let op = add;
     let i = op(c, d);
     print(i)
