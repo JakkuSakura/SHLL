a161a28b92e8f58949ec63ec04845379dc37db83
qiujiangkun <qjk2001@gmail.com>
2023-08-18 17:24:50 +0800

    [dev] deprecate old interpreter

--- rust-lang/examples/main_02_higher_kinded_function_gen.rs
+++ rust-lang/examples/main_02_higher_kinded_function_gen.rs
@@ -7,26 +7,26 @@ fn print(i: impl Display) {
     println!("{}", i)
 }
 fn main() {
-    let c = 3f64;
+    let a = 1i64;
     let b = 2i64;
+    let c = 3f64;
     let d = 4f64;
     let op = add;
-    let a = 1i64;
     {
-        let c = 3f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let d = 4f64;
         let op = add;
-        let a = 1i64;
         let i = op(a, b);
         print(i);
     };
     {
-        let c = 3f64;
+        let a = 1i64;
         let b = 2i64;
+        let c = 3f64;
         let d = 4f64;
         let op = add;
-        let a = 1i64;
         let i = op(c, d);
         print(i);
     };
