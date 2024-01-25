2df503fcbc1ced82eb5cbee10946c7780306a581
JakkuSakura <qjk2001@gmail.com>
2024-01-25 18:03:13 +0800

    refactor: refactor Deserializer

---
+++ common-lang/src/deserialize.rs
@@ -0,0 +1,11 @@
+use crate::ast::{Expr, File, Item, Tree};
+use crate::value::TypeValue;
+use common::*;
+
+pub trait Deserializer {
+    fn deserialize_tree(&self, code: &str) -> Result<Tree>;
+    fn deserialize_expr(&self, code: &str) -> Result<Expr>;
+    fn deserialize_item(&self, code: &str) -> Result<Item>;
+    fn deserialize_file(&self, path: &std::path::Path) -> Result<File>;
+    fn deserialize_type(&self, code: &str) -> Result<TypeValue>;
+}
