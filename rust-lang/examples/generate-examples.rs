4ee3201ec5fa378f5828409b341130de13625799
JakkuSakura <qjk2001@gmail.com>
2024-03-07 21:45:11 +0800

    [dev] bugfixes WIP

--- rust-lang/examples/generate-examples.rs
+++ rust-lang/examples/generate-examples.rs
@@ -1,15 +1,15 @@
 use common::*;
 
-use common_lang::context::ScopedContext;
+use common_lang::context::SharedScopedContext;
 use common_lang::interpreter::Interpreter;
-use common_lang::optimizer::load_optimizers;
+use common_lang::pass::load_optimizers;
 use common_lang::{Deserializer, Serializer};
+use itertools::Itertools;
 use rust_lang::printer::RustPrinter;
 use rust_lang::rustfmt::format_code;
 use rust_lang::RustSerde;
 use std::fs::File;
 use std::io::{Seek, SeekFrom, Write};
-use std::rc::Rc;
 use std::sync::Arc;
 
 fn main() -> Result<()> {
@@ -21,7 +21,7 @@ fn main() -> Result<()> {
         .map(|x| Ok::<_, Error>(x?.path()))
         .try_collect()?;
     dirs.sort();
-    let rust_serde = RustSerde::new();
+    let rust_serde = Arc::new(RustSerde::new());
     for file_in in dirs {
         let file_str = file_in.file_name().unwrap().to_string_lossy().to_string();
         if !file_str.contains("main_") || file_str.contains("_gen.rs") {
@@ -34,8 +34,8 @@ fn main() -> Result<()> {
         let mut file_out = File::create(file_out)?;
         let file_content = std::fs::read_to_string(file_in)?;
         let mut node = rust_serde.deserialize_tree(&file_content)?;
-        let ctx = Arc::new(ScopedContext::new());
-        let optimizers = load_optimizers(Rc::new(rust_serde));
+        let ctx = SharedScopedContext::new();
+        let optimizers = load_optimizers(rust_serde.clone() as _);
         for optimizer in optimizers {
             node = optimizer.optimize_tree(node, &ctx)?;
         }
@@ -46,8 +46,8 @@ fn main() -> Result<()> {
         file_out.seek(SeekFrom::Start(0))?;
         writeln!(&mut file_out, "{}", code)?;
 
-        let inp = Interpreter::new(Rc::new(rust_serde));
-        let ctx = Arc::new(ScopedContext::new());
+        let inp = Interpreter::new(rust_serde.clone() as _);
+        let ctx = SharedScopedContext::new();
         let intp_result = inp.interpret_tree(node, &ctx)?;
         for row in ctx.take_outputs() {
             writeln!(&mut file_out, "// stdout: {}", row)?;
--- rust-lang/examples/main_01_specialization_gen.rs
+++ rust-lang/examples/main_01_specialization_gen.rs
@@ -1,3 +1,9 @@
+fn inc(i: i64) -> i64 {
+    i + 1 + 2 + 3
+}
+fn double(i: i64) -> i64 {
+    i * 2
+}
 fn print(i: i64) {
     println!("{}", i)
 }
