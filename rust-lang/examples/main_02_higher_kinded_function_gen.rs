d407f7cee5796916423456f6f88e25091bc2f049
JakkuSakura <qjk2001@gmail.com>
2024-03-07 21:45:11 +0800

    fix: bugfixes WIP

--- rust-lang/examples/main_02_higher_kinded_function_gen.rs
+++ rust-lang/examples/main_02_higher_kinded_function_gen.rs
@@ -1,7 +1,17 @@
 use std::fmt::Display;
 use std::ops::Add;
+fn do_op<T: Add>(a: i64, b: i64, c: f64, d: f64, op: fn(T, T) -> T) {
+    print2(op(a, b));
+    print2(op(c, d));
+}
+fn add<T: Add>(a: T, b: T) -> T {
+    a + b
+}
 fn print(i: impl Display) {
     println!("{}", i)
+}
+fn print2(i: impl Display) {
+    print(i);
 }
 fn main() {
     do_op(1, 2, 3, 4, add);
