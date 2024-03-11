eb19e932bb6eecd8e0048f66f9ad273bfc6389e0
JakkuSakura <qjk2001@gmail.com>
2024-03-11 18:28:46 +0800

    chore: recycle registers

--- lang-mips/src/emitter/mod.rs
+++ lang-mips/src/emitter/mod.rs
@@ -1,38 +1,20 @@
+use std::rc::Rc;
+
 use crate::emitter::expr::MipsEmitExprResult;
 use crate::instruction::MipsInstruction;
-use crate::register::MipsRegister;
+use crate::register::RegisterManager;
 
 mod expr;
 
 pub struct MipsEmitter {
-    register_s: Vec<MipsRegister>,
-    register_t: Vec<MipsRegister>,
+    register: Rc<RegisterManager>,
     label_counter: usize,
 }
 
 impl MipsEmitter {
     pub fn new() -> Self {
         Self {
-            register_s: vec![
-                MipsRegister::S1,
-                MipsRegister::S2,
-                MipsRegister::S3,
-                MipsRegister::S4,
-                MipsRegister::S5,
-                MipsRegister::S6,
-                MipsRegister::S7,
-            ],
-            register_t: vec![
-                MipsRegister::T0,
-                MipsRegister::T1,
-                MipsRegister::T2,
-                MipsRegister::T3,
-                MipsRegister::T4,
-                MipsRegister::T5,
-                MipsRegister::T6,
-                MipsRegister::T7,
-                MipsRegister::T8,
-            ],
+            register: Rc::new(RegisterManager::new()),
             label_counter: 0,
         }
     }
@@ -41,24 +23,13 @@ impl MipsEmitter {
         self.label_counter += 1;
         label
     }
-    pub fn get_register_t(&mut self) -> MipsRegister {
-        self.register_t.pop().unwrap()
-    }
-    pub fn get_register_s(&mut self) -> MipsRegister {
-        self.register_s.pop().unwrap()
-    }
-    pub fn return_register(&mut self, reg: MipsRegister) {
-        if reg.is_s() {
-            self.register_s.push(reg);
-        } else if reg.is_t() {
-            self.register_t.push(reg);
-        }
-    }
 
     // MIPS only allows 16-bit immediate values, so we need to use the `li` instruction to load
-    pub fn emit_load_immediate(&self, ret: MipsRegister, value: i16) -> MipsEmitExprResult {
+    pub fn emit_load_immediate(&self, value: i16) -> MipsEmitExprResult {
+        let ret = self.register.acquire_t().unwrap();
+
         let ins = MipsInstruction::Li {
-            rt: ret,
+            rt: ret.register,
             immediate: value,
         };
         MipsEmitExprResult {
