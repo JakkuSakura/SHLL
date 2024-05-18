98fb355eb14db7a7b40f126f72d9f4b0548f9d95
JakkuSakura <qjk2001@gmail.com>
2024-05-18 11:35:06 +0800

    feat: pull in tree-sitter

---
+++ lang-core/src/cst/ts.rs
@@ -0,0 +1,8 @@
+pub use tree_sitter::Language as TreeSitterLanguage;
+pub use tree_sitter::Node as TreeSitterNode;
+pub use tree_sitter::Parser as TreeSitterParser;
+pub use tree_sitter::Query as TreeSitterQuery;
+pub use tree_sitter::QueryCursor as TreeSitterQueryCursor;
+pub use tree_sitter::Range as TreeSitterRange;
+pub use tree_sitter::Tree as TreeSitterTree;
+pub use tree_sitter::TreeCursor as TreeSitterTreeCursor;
