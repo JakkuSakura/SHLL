f4dcd21d2794431b69a01bc81d315ef64a897d25
JakkuSakura <qjk2001@gmail.com>
2024-07-12 12:38:29 +0800

    feat: refactor lang-core

--- lang-core/src/ast/attr/mod.rs
+++ lang-core/src/ast/attr/mod.rs
@@ -34,3 +34,27 @@ common_struct! {
         pub meta: AstAttrMeta,
     }
 }
+
+pub trait AstAttributesExt {
+    fn find_by_path(&self, path: &Path) -> Option<&AstAttrMeta>;
+    fn find_by_name(&self, name: &str) -> Option<&AstAttrMeta>;
+}
+impl AstAttributesExt for Vec<AstAttribute> {
+    fn find_by_path(&self, path: &Path) -> Option<&AstAttrMeta> {
+        self.iter()
+            .find(|x| match &x.meta {
+                AstAttrMeta::Path(p) => p == path,
+                _ => false,
+            })
+            .map(|x| &x.meta)
+    }
+    fn find_by_name(&self, name: &str) -> Option<&AstAttrMeta> {
+        self.iter()
+            .find(|x| match &x.meta {
+                AstAttrMeta::NameValue(nv) => nv.name.last().as_str() == name,
+                AstAttrMeta::Path(p) => p.last().as_str() == name,
+                _ => false,
+            })
+            .map(|x| &x.meta)
+    }
+}
